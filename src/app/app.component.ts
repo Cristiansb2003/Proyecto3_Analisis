c61a1f17f2084112d781407eacaa75589f9f8197
cristian <cristian192003@outlook.es>
2023-10-04 23:42:53 -0600

    Antes de agregar bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto-3';
+  cantidad: number = 1;
+  valorX!: number;
+  valorY!: number;
+  paresXY: { x: number, y: number }[] = [];
+
+  agregarPar() {
+    if (this.valorX !== undefined && this.valorY !== undefined) {
+      this.paresXY.push({ x: this.valorX, y: this.valorY });
+      // Limpia los valores de X e Y después de agregarlos al arreglo
+      this.valorX = 0;
+      this.valorY = 0;
+      console.log("hol")
+    }
+  }
 }
